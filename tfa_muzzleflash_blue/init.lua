bb7a35bb727e456b722a939b8329182781b63a60
AlexBroNFK <alexbronfk16161@gmail.com>
2024-09-20 18:11:44 +0500

    Add files via upload

---
+++ tfa_muzzleflash_blue/init.lua
@@ -0,0 +1,10 @@
+include("tfa/muzzleflash_base.lua")
+
+EFFECT.HeatSize = 0.5
+EFFECT.FlashSize = 0.25
+EFFECT.SparkSize = 0.35
+EFFECT.Color = Color(150,150,255)
+EFFECT.Life = 0.15
+EFFECT.XFlashSize = 0.1
+EFFECT.SmokeSize = 0
+EFFECT.ColorSprites = true
